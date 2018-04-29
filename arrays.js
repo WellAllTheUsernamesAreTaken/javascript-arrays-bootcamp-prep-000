dbc9265b11be1646e2659768537b12fbc3123570
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-29 19:54:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function accessElementInArray(array, index) {
 
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.slice();
+  return array.slice(1);
 }
 function removeElementFromBeginningOfArray(array) {
   var a = array.slice(1);
