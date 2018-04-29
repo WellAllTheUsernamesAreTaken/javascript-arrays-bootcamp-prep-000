464c0910f6169fa5419b1f45570f744bf8468432
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-29 19:04:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(yourArray, element) {
   var myArray = new Array();
-  myArray = yourArray;
+  myArray = yourArray();
   return myArray.unshift(element)
 }
 function destructivelyAddElementToBeginningOfArray() {
