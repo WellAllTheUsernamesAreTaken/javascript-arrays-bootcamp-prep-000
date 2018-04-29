c7eae9fa3936c750d8b64b9053949113af85ca08
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-29 19:17:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,16 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return array.unshift(element);
 }
 function addElementToEndOfArray() {
   
 }
 function destructivelyAddElementToEndOfArray() {
-  chocolateBars = [...chocolateBars, "Hersey"]
+  chocolateBars = [...chocolateBars, "Hersey"];
 }
 
 
