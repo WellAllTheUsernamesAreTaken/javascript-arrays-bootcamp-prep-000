5627c80a8473f2565716e7b34660943de037cfbf
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-29 19:49:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,13 +4,13 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(`'${element}'`);
+  return array.unshift(`${element}`);
 }
 function addElementToEndOfArray(array, element) {
   return [...array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(`'${element}'`);
+  return array.push(`${element}`);
 }
 
 
