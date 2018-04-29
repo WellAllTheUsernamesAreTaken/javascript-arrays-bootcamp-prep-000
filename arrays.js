9de487965edb487424bcde05e9a925c942766059
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-29 20:01:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,7 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var a = array;
-  a = a.unshift(`${element}`);
+  const a = array.unshift(`${element}`);
   return a;
 }
 function addElementToEndOfArray(array, element) {
@@ -22,11 +21,11 @@ function accessElementInArray(array, index) {
 
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  var a = array.slice(1);
+  const a = array.slice(1);
   return a;
 }
 function removeElementFromBeginningOfArray(array) {
-  var a = array.slice(1);
+  const a = array.slice(1);
   return a;
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
